3edc5dd57d3683183e2258bf71903e243225275b
Ed Scadding <edscadding@secondfiddle.org.uk>
2013-07-23 09:51:15 +0100

    Added string mappings

--- plugins/uk.org.secondfiddle.pep.projects/src/uk/org/secondfiddle/pep/projects/wizard/ProjectTemplateSection.java
+++ plugins/uk.org.secondfiddle.pep.projects/src/uk/org/secondfiddle/pep/projects/wizard/ProjectTemplateSection.java
@@ -12,6 +12,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.ResourceBundle;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.WordUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.preferences.InstanceScope;
@@ -24,6 +26,7 @@ import org.eclipse.pde.ui.templates.OptionTemplateWizardPage;
 import org.eclipse.pde.ui.templates.TemplateOption;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
+import uk.org.secondfiddle.pep.projects.ProjectTemplateActivator;
 import uk.org.secondfiddle.pep.projects.model.ParameterDescriptor;
 import uk.org.secondfiddle.pep.projects.model.ParameterMapping;
 import uk.org.secondfiddle.pep.projects.model.ParameterPreference;
@@ -32,6 +35,10 @@ import uk.org.secondfiddle.pep.projects.model.ProjectTemplate;
 public class ProjectTemplateSection extends BaseOptionTemplateSection {
 
 	private static final String UNMAPPED_VALUE_SUFFIX = ":unmapped";
+
+	private static final String UPPER_CAMEL_CASE_SUFFIX = ":uppercamelcase";
+
+	private static final String LOWER_CAMEL_CASE_SUFFIX = ":lowercamelcase";
 
 	private static final String TEMPLATE_DIRECTORY_NAME = "template";
 
@@ -65,7 +72,7 @@ public class ProjectTemplateSection extends BaseOptionTemplateSection {
 		 * below is deliberate
 		 */
 		String replacement = super.getReplacementString(fileName, key);
-		return (replacement == key ? replacementStrings.get(key) : replacement);
+		return (replacement == key ? getValueString(key) : replacement);
 	}
 
 	@Override
@@ -83,9 +90,15 @@ public class ProjectTemplateSection extends BaseOptionTemplateSection {
 	}
 
 	@Override
-	public URL getTemplateLocation() {
+	protected void generateFiles(IProgressMonitor monitor) throws CoreException {
+		for (String location : template.getLocations()) {
+			generateFiles(monitor, toTemplateLocationURL(location));
+		}
+	}
+
+	private URL toTemplateLocationURL(String location) {
 		try {
-			File templateDir = new File(template.getPrimaryLocation(), TEMPLATE_DIRECTORY_NAME);
+			File templateDir = new File(location, TEMPLATE_DIRECTORY_NAME);
 			return templateDir.toURI().toURL();
 		} catch (MalformedURLException e) {
 			throw new RuntimeException(e);
@@ -138,6 +151,8 @@ public class ProjectTemplateSection extends BaseOptionTemplateSection {
 			String newValue = valueString.replaceAll(valueMapping.getPattern(), valueMapping.getReplacement());
 			templateOption.setValue(newValue);
 
+			addStringMappings(templateOption.getName(), valueString);
+
 			ParameterPreference preference = descriptor.getPreference();
 			if (preference != null) {
 				IPersistentPreferenceStore preferences = new ScopedPreferenceStore(InstanceScope.INSTANCE,
@@ -146,10 +161,18 @@ public class ProjectTemplateSection extends BaseOptionTemplateSection {
 				try {
 					preferences.save();
 				} catch (IOException e) {
-					throw new RuntimeException(e);
+					ProjectTemplateActivator.logError("Failed to save preference", e);
 				}
 			}
 		}
+	}
+
+	private void addStringMappings(String name, String value) {
+		String capitalized = WordUtils.capitalizeFully(value);
+		String upperCamel = StringUtils.deleteWhitespace(capitalized);
+		String lowerCamel = StringUtils.uncapitalize(upperCamel);
+		replacementStrings.put(name + LOWER_CAMEL_CASE_SUFFIX, lowerCamel);
+		replacementStrings.put(name + UPPER_CAMEL_CASE_SUFFIX, upperCamel);
 	}
 
 	@Override
